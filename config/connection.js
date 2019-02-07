310cc0e263528f7d7e25a59af762d5fd769d807c
Matt <mvt3641@gmail.com>
2019-02-06 22:35:21 -0500

    pushing to heroku

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,14 @@
 
 
-var mongoose =require('mongoose');
-
-mongoose.Promise = global.Promise;
-var dbc;
-// mongoose.Promise = Promise;
-var connection = mongoose.connect("mongodb://localhost:27017/test")
-.then(function(){
-console.log("database connected..");
-});
-
-
-  module.exports = connection;
+// var mongoose =require('mongoose');
+//
+// mongoose.Promise = global.Promise;
+// var dbc;
+// // mongoose.Promise = Promise;
+// var connection = mongoose.connect("mongodb://localhost:27017/test")
+// .then(function(){
+// console.log("database connected..");
+// });
+//
+//
+//   module.exports = connection;
