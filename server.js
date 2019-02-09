c21c7821b6a30d0ac547f03165f3d828480333fc
Matt <mvt3641@gmail.com>
2019-02-09 17:38:29 -0500

    updated cards and navbar

--- server.js
+++ server.js
@@ -43,6 +43,10 @@ app.get('/about', (req, res)=>{
 res.sendFile(path.join(__dirname, "/public/about.html"));
 });
 
+app.get('/contact', (req, res)=>{
+res.sendFile(path.join(__dirname, "/public/contact.html"));
+});
+
 // app.get('/upl',function(req, res){
 //   res.sendFile(path.join(__dirname, "/public/uploadpage.html"));
 // });
